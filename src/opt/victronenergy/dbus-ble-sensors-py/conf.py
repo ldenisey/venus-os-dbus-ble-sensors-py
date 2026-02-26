b4d12d092ae25086be5c1e1539140e2fc2486c5b
ldenisey <ldenisey@gmail.com>
2026-02-26 22:09:00 +0000

    Updating man id file

--- src/opt/victronenergy/dbus-ble-sensors-py/conf.py
+++ src/opt/victronenergy/dbus-ble-sensors-py/conf.py
@@ -3,7 +3,7 @@ import os
 
 # Project variables
 PROCESS_NAME = os.path.basename(os.path.dirname(__file__))
-PROCESS_VERSION = '1.1.0'
+PROCESS_VERSION = '1.1.1'
 
 # Timeouts
 IGNORED_DEVICES_TIMEOUT = 600  # 10 min
